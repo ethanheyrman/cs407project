249a687a7c5ff8f32c89e4852ae7cd6300c8b4bc
Ethan Heyrman <eheyrman@wisc.edu>
2020-04-14 17:55:48 -0500

    landing and nav

---
+++ app/src/main/java/com/example/cs407project/AuthenticationActivity.java
@@ -0,0 +1,87 @@
+package com.example.cs407project;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class AuthenticationActivity extends AppCompatActivity {
+    private static final String TAG = "Authentication";
+
+    private EditText emailField;
+    private EditText passwordField;
+    private Button loginButton;
+    private Button createAccount;
+
+    private FirebaseAuth mAuth;
+
+    private FirebaseUser currentUser;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authentication);
+
+        mAuth = FirebaseAuth.getInstance();
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        currentUser = mAuth.getCurrentUser();
+        navigateToHome(currentUser);
+
+    }
+
+    private void createAccount(String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            navigateToHome(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(AuthenticationActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+//                            updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+    public void onLoginClick(View view) {
+        navigateToHome(currentUser);
+    }
+
+    public void navigateToHome(FirebaseUser user) {
+        Intent intent = new Intent(this,
+                com.example.cs407project.HomeActivity.class);
+        startActivity(intent);
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/cs407project/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.example.cs407project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+}
