e4a9fe3b5b50f20109fdb29c00786246131c30a0
Ryan Martinez <RyanM0126@github.com>
2020-04-28 17:18:35 -0500

    added sms redirect

--- app/src/main/java/com/example/cs407project/HomeActivity.java
+++ app/src/main/java/com/example/cs407project/HomeActivity.java
@@ -4,10 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
@@ -20,10 +23,12 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PolylineOptions;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
 public class HomeActivity extends AppCompatActivity implements GoogleMap.OnMarkerClickListener, OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private Marker mTest;
     private FusedLocationProviderClient mFusedLocationProviderClient; //save the instance
     private final LatLng mDestinationLatLng = new LatLng(43.0715255, -89.4088546);
     private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 12; //could've been any number
@@ -41,6 +46,68 @@ public class HomeActivity extends AppCompatActivity implements GoogleMap.OnMarke
         mapFragment.getMapAsync(this);
         mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
 
+    }
+
+    private void displayMyLocation() {
+        //Check if permission granted
+        int permission = ActivityCompat.checkSelfPermission(this.getApplicationContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION);
+        //If not, ask for it
+        if (permission == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+        //If permission granted, display marker at current location
+        else {
+            mFusedLocationProviderClient.getLastLocation()
+                    .addOnCompleteListener(this, task -> {
+                        Location mLastKnownLocation = task.getResult();
+                        if (task.isSuccessful() && mLastKnownLocation != null) {
+                            System.out.println("This is the last known location: " + mLastKnownLocation);
+                            mMap.addPolyline(new PolylineOptions()
+                                    .add(new LatLng(mLastKnownLocation.getLatitude(),
+                                            mLastKnownLocation.getLongitude()), mDestinationLatLng));
+                        }
+                    });
+        }
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        mTest = mMap.addMarker(new MarkerOptions()
+                .position(mDestinationLatLng)
+                .title("test destination"));
+        displayMyLocation();
+        mMap.setOnMarkerClickListener(this);
+    }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        MaterialAlertDialogBuilder selectionWindow = new MaterialAlertDialogBuilder(this)
+                .setTitle("Item Selected")
+                .setMessage("Test Info")
+                .setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                })
+                .setPositiveButton("MESSAGE", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent messagingIntent = new Intent(Intent.ACTION_SENDTO);
+                        messagingIntent.setData(Uri.parse("smsto:" + Uri.encode("18002221111")));
+                        messagingIntent.putExtra("sms_body", "Hello! I would like to provide/request PPE");
+                        startActivity(messagingIntent);
+                    }
+                });
+        selectionWindow.show();
+
+        return false;
     }
 
 
@@ -71,39 +138,4 @@ public class HomeActivity extends AppCompatActivity implements GoogleMap.OnMarke
                         return false;
                 }
             };
-
-    private void displayMyLocation() {
-        //Check if permission granted
-        int permission = ActivityCompat.checkSelfPermission(this.getApplicationContext(),
-                Manifest.permission.ACCESS_FINE_LOCATION);
-        //If not, ask for it
-        if (permission == PackageManager.PERMISSION_DENIED) {
-            ActivityCompat.requestPermissions(this,
-                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
-                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
-        }
-        //If permission granted, display marker at current location
-        else {
-            mFusedLocationProviderClient.getLastLocation()
-                    .addOnCompleteListener(this, task -> {
-                        Location mLastKnownLocation = task.getResult();
-                        if (task.isSuccessful() && mLastKnownLocation != null) {
-                            System.out.println("This is the last known location: " + mLastKnownLocation);
-                            mMap.addPolyline(new PolylineOptions()
-                                    .add(new LatLng(mLastKnownLocation.getLatitude(),
-                                            mLastKnownLocation.getLongitude()), mDestinationLatLng));
-                        }
-                    });
-        }
-    }
-
-    @Override
-    public boolean onMarkerClick(Marker marker) {
-        return false;
-    }
-
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-
-    }
 }
